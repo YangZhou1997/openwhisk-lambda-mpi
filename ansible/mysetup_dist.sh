d1c211e6322d468f5cd682435834918823c9e645
YangZhou1997 <yangzhou@g.harvard.edu>
2019-05-02 13:55:53 -0600

    try to dist deploy openwhisk

--- ansible/mysetup_dist.sh
+++ ansible/mysetup_dist.sh
@@ -3,38 +3,30 @@ ansible-playbook setup.yml
 
 # Building
 cd ..
-./gradlew distDocker
+./gradlew distDocker -PdockerRegistry=node-1.mesh-five-nodes.lambda-mpi-pg0.wisc.cloudlab.us:444
 cd ansible
 
 sudo chown -R yangzhou:lambda-mpi-PG0 tmp/
 sudo chown -R yangzhou:lambda-mpi-PG0 ../bin/wsk
 
 
-echo "prereq.yml"
 ansible-playbook prereq.yml
 
-echo "couchdb.yml"
 ansible-playbook couchdb.yml
-echo "initdb.yml"
 ansible-playbook initdb.yml
-echo "wipe.yml"
 ansible-playbook wipe.yml
-echo "openwhisk.yml"
 ansible-playbook openwhisk.yml
 
 # installs a catalog of public packages and actions
-echo "postdeploy.yml"
 ansible-playbook postdeploy.yml
 
 # to use the API gateway
-echo "apigateway.yml"
 ansible-playbook apigateway.yml
-echo "routemgmt.yml"
 ansible-playbook routemgmt.yml
 
 
 # set wsk
-wsk property set --apihost 10.10.1.2
+wsk property set --apihost 10.10.1.5
 wsk property set --auth `cat ~/openwhisk/ansible/files/auth.whisk.system`
 
 wsk sdk install bashauto
