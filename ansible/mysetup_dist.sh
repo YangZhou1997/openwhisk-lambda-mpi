ad8edbdc1fd6054d7eb924b30b3a9578de421cd5
YangZhou1997 <yangzhou@g.harvard.edu>
2019-05-12 18:57:29 -0600

    restore default network setting, building successfully

--- ansible/mysetup_dist.sh
+++ ansible/mysetup_dist.sh
@@ -4,7 +4,7 @@ docker container stop registry && docker container rm -v registry
 
 docker run -d --restart=always --name registry -v /users/yangzhou/certs:/certs -e REGISTRY_HTTP_ADDR=0.0.0.0:444 -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt -e REGISTRY_HTTP_TLS_KEY=/certs/domain.key -p 444:444 registry:2
 
-OW_HOME=/users/yangzhou/openwhisk-ori
+OW_HOME=/users/yangzhou/openwhisk
 
 # Building
 cd ..
