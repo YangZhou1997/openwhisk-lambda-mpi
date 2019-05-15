204affb5dadc1a732847850138f3d54fc2e27692
YangZhou1997 <yangzhou@g.harvard.edu>
2019-05-15 17:36:03 -0600

    add testing  code

---
+++ CS260Tests/run_ow_overhead.sh
@@ -0,0 +1,7 @@
+# !/bin/bash
+
+for fibNum in 1 2 4 8 16 32 64 128 256 ; do
+    echo $fibNum
+    python test-overhead.py $fibNum >> ori_ow_results.txt
+done
+
--- CS260Tests/test-overhead.py
+++ CS260Tests/test-overhead.py
@@ -9,6 +9,7 @@ import random
 import time
 import string
 import json as simplejson
+import sys
 
 class wsk_fib:
     def __init__(self, fib_num, number):
@@ -41,7 +42,7 @@ class wsk_fib:
 
         # ok: invoked /_/fib with id 046616d8795d43eaa616d8795db3ea95
         activationID = returned_value.decode("utf-8").split(' ')[5].rstrip()
-        print(activationID)
+        # print(activationID)
 
         self.fib_id[work_id] = activationID
 
@@ -50,34 +51,86 @@ class wsk_fib:
         for i in range(0, self.fib_num):
             self.start_function_instance(i)
 
-        time.sleep(150 * 4)
+        time.sleep(300)
 
         for i in range(0, self.fib_num):
-            returned_value = subprocess.check_output("wsk -i activation get %s | sed '1d'" % (self.fib_id[i]), shell=True)
-            results = json.loads(returned_value.decode("utf-8"))
-            self.fib_log[i] = results
-            # print(results)
+            try:
+                returned_value = subprocess.check_output("wsk -i activation get %s | sed '1d'" % (self.fib_id[i]), shell=True)
+            except subprocess.CalledProcessError as e:
+                # print(self.fib_id[i])
+                # print(e.output)
+                continue
+
+            if returned_value == None:
+                continue
+            try:
+                results = json.loads(returned_value.decode("utf-8"))
+                self.fib_log[i] = results
+                # print(results)
+            except json.decoder.JSONDecodeError as e:
+                continue
 
 
 if __name__ == "__main__":
-    wsk_ts_test = wsk_fib(64 * 2, 39)
+    num_fib = int(sys.argv[1])
+    wsk_ts_test = wsk_fib(num_fib, 39)
     wsk_ts_test.create_func()
     wsk_ts_test.start_fibs()
-    duration = 0.0
+
+    usertime = 0.0
+    systime = 0.0
+    inittime = 0.0
+
+    usertimeArray = []
+    systimeArray = []
+    inittimeArray = []
+
+    error_count = 0
+    warm_start = 0
+
     f = open("annotations.txt", 'w')
     for i in range(0, wsk_ts_test.fib_num):
-        print(wsk_ts_test.fib_log[i]['response']['result']['fibonacci'])
-        print(len(wsk_ts_test.fib_log[i]['annotations']))
         simplejson.dump(wsk_ts_test.fib_log[i], f)
         f.write("----------------------------------------------\n")
-        duration += wsk_ts_test.fib_log[i]['duration']
-    print(duration / wsk_ts_test.fib_num)
+
+        if wsk_ts_test.fib_log[i] == None:
+            error_count += 1
+            # print('activation error for %s' % (wsk_ts_test.fib_id[i]))
+        elif 'fibonacci' not in wsk_ts_test.fib_log[i]['response']['result']:
+            error_count += 1
+            # print('%s for %s' % (wsk_ts_test.fib_log[i]['response']['result'], wsk_ts_test.fib_id[i]))
+        else:
+            startType = ['warm', 'cold'][len(wsk_ts_test.fib_log[i]['annotations']) == 6]
+            # print('fib results: %d; %s' % (wsk_ts_test.fib_log[i]['response']['result']['fibonacci'], startType))
+
+            temp = wsk_ts_test.fib_log[i]['duration']
+            usertimeArray.append(temp)
+            usertime += temp
+
+            temp = wsk_ts_test.fib_log[i]['annotations'][1]['value']
+            systimeArray.append(temp)
+            systime += temp
+
+            if startType == 'cold':
+                temp = wsk_ts_test.fib_log[i]['annotations'][5]['value']
+                inittimeArray.append(temp)
+                inittime += temp
+                warm_start += 1
+
+
+    print('average waiting time: %f' % (systime / (wsk_ts_test.fib_num - error_count)))
+    print(systimeArray)
+
+    print('average execution time: %f' % (usertime / (wsk_ts_test.fib_num - error_count)))
+    print(usertimeArray)
+
+    print('average init time: %f' % (inittime / warm_start))
+
+    print('successful instances number: %s' % (wsk_ts_test.fib_num - error_count))
     f.close()
 
 
     # Does not work??
     # for fiblog in wsk_ts_test.fib_log:
     #     print(fiblog['duration'])
-
-
 
