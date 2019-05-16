93dceca0dd12995e204b54f3ca900b9f4fad3ccf
YangZhou1997 <yangzhou@g.harvard.edu>
2019-05-16 09:11:07 -0600

    fix path for addrMap

--- CS260Tests/test-data-transferal.py
+++ CS260Tests/test-data-transferal.py
@@ -1,110 +1,136 @@
-import socket
+#! /bin/python3
+
+import subprocess
+from threading import Thread
+from threading import Lock
 import os
-import time
-import sys
-from builtins import bytes
+import json
 import random
+import time
 import string
-import struct
+import json as simplejson
+import sys
 
-PORT = 65432
+class wsk_data:
+    def __init__(self, fib_num, number):
+        self.fib_num = fib_num
+        self.finished_fib_num = 0
+        self.fib_para = number
 
-def get_peerips():
+        self.lock = Lock()
+        self.fib_id = [None] * 3600
+        self.fib_log = [None] * 3600
 
-    f = open("/addrMap/addrMap.txt", 'r')
-    idipStr = f.readline().strip('\n')
+        # self.funcName = ['test_a','test_b','test_c','test_d',
+        #                  'test_e','test_f','test_g','test_h',
+        #                  'test_i','test_j','test_k','test_l',
+        #                  'test_l','test_m','test_n','test_o']
+
+        self.funcName = [''.join(random.sample(string.ascii_letters + string.digits, 8)) for i in range(16)]
+
+
+    def create_func(self):
+        for i in range(0, len(self.funcName)):
+            # subprocess.check_output("wsk -i action update -t 300000 --docker yangzhou1997/python3action:mpi %s ~/openwhisk/functions/test-random/fib.py" % (self.funcName[i]), shell=True)
+            subprocess.check_output("wsk -i action update -t 300000 %s ~/openwhisk/functions/test-random/fib.py" % (self.funcName[i]), shell=True)
+
+
+    def start_function_instance(self, work_id):
+        random_func = self.funcName[random.randint(0,len(self.funcName) - 1)]
+
+        returned_value = subprocess.check_output("wsk -i action invoke %s -p number %d -p instanceID %s" % (random_func, self.fib_para, ''.join(random.sample(string.ascii_letters + string.digits, 8))), shell=True)
+
+        # ok: invoked /_/fib with id 046616d8795d43eaa616d8795db3ea95
+        activationID = returned_value.decode("utf-8").split(' ')[5].rstrip()
+        # print(activationID)
+
+        self.fib_id[work_id] = activationID
+
+
+    def start_fibs(self):
+        for i in range(0, self.fib_num):
+            self.start_function_instance(i)
+
+        time.sleep(300)
+
+        for i in range(0, self.fib_num):
+            try:
+                returned_value = subprocess.check_output("wsk -i activation get %s | sed '1d'" % (self.fib_id[i]), shell=True)
+            except subprocess.CalledProcessError as e:
+                # print(self.fib_id[i])
+                # print(e.output)
+                continue
+
+            if returned_value == None:
+                continue
+            try:
+                results = json.loads(returned_value.decode("utf-8"))
+                self.fib_log[i] = results
+                # print(results)
+            except json.decoder.JSONDecodeError as e:
+                continue
+
+
+if __name__ == "__main__":
+    num_fib = int(sys.argv[1])
+    wsk_ts_test = wsk_data(num_fib, 39)
+    wsk_ts_test.create_func()
+    wsk_ts_test.start_fibs()
+
+    usertime = 0.0
+    systime = 0.0
+    inittime = 0.0
+
+    usertimeArray = []
+    systimeArray = []
+    inittimeArray = []
+
+    error_count = 0
+    warm_start = 0
+
+    f = open("annotations.txt", 'w')
+    for i in range(0, wsk_ts_test.fib_num):
+        simplejson.dump(wsk_ts_test.fib_log[i], f)
+        f.write("----------------------------------------------\n")
+
+        if wsk_ts_test.fib_log[i] == None:
+            error_count += 1
+            # print('activation error for %s' % (wsk_ts_test.fib_id[i]))
+        elif 'fibonacci' not in wsk_ts_test.fib_log[i]['response']['result']:
+            error_count += 1
+            # print('%s for %s' % (wsk_ts_test.fib_log[i]['response']['result'], wsk_ts_test.fib_id[i]))
+        else:
+            startType = ['warm', 'cold'][len(wsk_ts_test.fib_log[i]['annotations']) == 6]
+            # print('fib results: %d; %s' % (wsk_ts_test.fib_log[i]['response']['result']['fibonacci'], startType))
+
+            temp = wsk_ts_test.fib_log[i]['duration']
+            usertimeArray.append(temp)
+            usertime += temp
+
+            temp = wsk_ts_test.fib_log[i]['annotations'][1]['value']
+            systimeArray.append(temp)
+            systime += temp
+
+            if startType == 'cold':
+                temp = wsk_ts_test.fib_log[i]['annotations'][5]['value']
+                inittimeArray.append(temp)
+                inittime += temp
+                warm_start += 1
+
+
+    print('average waiting time: %f' % (systime / (wsk_ts_test.fib_num - error_count)))
+    print(systimeArray)
+
+    print('average execution time: %f' % (usertime / (wsk_ts_test.fib_num - error_count)))
+    print(usertimeArray)
+
+    print('average init time: %f' % (inittime / warm_start))
+
+    print('successful instances number: %s' % (wsk_ts_test.fib_num - error_count))
     f.close()
 
 
-    idipPairs = idipStr.split("&")
-    idipMap = dict()
-    for idip in idipPairs:
-        temp = idip.split("=")
-        idipMap[temp[0]] = temp[1]
-    print(idipMap)
-    return idipMap
-
-def send_msg(sock, msg):
-    # Prefix each message with a 4-byte length (network byte order)
-    msg = struct.pack('>I', len(msg)) + msg
-    sock.sendall(msg)
-
-def recv_msg(sock):
-    # Read message length and unpack it into an integer
-    raw_msglen = recvall(sock, 4)
-    if not raw_msglen:
-        return None
-    msglen = struct.unpack('>I', raw_msglen)[0]
-    # Read the message data
-    return recvall(sock, msglen)
-
-def recvall(sock, n):
-    # Helper function to recv n bytes or return None if EOF is hit
-    data = b''
-    while len(data) < n:
-        packet = sock.recv(n - len(data))
-        if not packet:
-            return None
-        data += packet
-    return data
-
-def main(args):
-    time.sleep(3)
-
-    dataSize = int(args.get("dataSize")) # 1 (MB)
-
-    selfid = args.get("instanceID") # myname0 or myname1: String
-    if selfid == "myname0":
-        peerid = "myname1"
-    else:
-        peerid = "myname0"
-
-
-    idipMap = get_peerips()
-    peerip = idipMap[peerid]
-    selfip = idipMap[selfid]
-
-    socket_type = selfid
-    if socket_type == "myname1": # client
-        time.sleep(1)
-
-        data = None
-        base_sending_data = ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(dataSize)])
-
-        sending_data = ""
-        for i in range(0, 1024 * 1024):
-            sending_data += base_sending_data
-        sending_data = bytes(sending_data, encoding = "utf8")
-
-        print("client: ")
-        time1 = time.time()
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((peerip, PORT))
-        send_msg(s, sending_data)
-        data = recv_msg(s)
-        time2 = time.time()
-        s.close()
-        return {"res": time2 - time1}
-
-    else: # server
-        print("server: ")
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((selfip, PORT))
-        s.listen()
-        conn, addr = s.accept()
-        print('Connected by', addr)
-        while True:
-            data = recv_msg(conn)
-            print(data)
-            if not data:
-                conn.close()
-                break
-            send_msg(conn, data)
-        return {"res": 0}
-
-
-# python test-socket.py 1 -> test client
-# python test-socket.py 1 -> test server
-# must be removed when creating action in wsk
-# print(main({"client": sys.argv[1]}))
+    # Does not work??
+    # for fiblog in wsk_ts_test.fib_log:
+    #     print(fiblog['duration'])
 
