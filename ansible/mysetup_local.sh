43a1ae62056ca4fae489e15fa0bed8ecca4311b2
davey <davey@node-0.yangzhou-qv52374.lambda-mpi-pg0.clemson.cloudlab.us>
2019-05-16 08:28:49 -0600

    local setup

--- ansible/mysetup_local.sh
+++ ansible/mysetup_local.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+[ -f "environments/local/ansible_local.cfg" ] && cp environments/local/ansible_local.cfg ansible.cfg
+
 # Select a data store
 ansible-playbook setup.yml
 ansible-playbook prereq.yml
+
+./mysetup_cli.sh
+
+./mysetup_swarm_local.sh
 
 # Building
 cd ..
 ./gradlew distDocker
 cd ansible
-
-[ -f "environments/local/ansible_local.cfg" ] && cp environments/local/ansible_local.cfg ansible.cfg
-
 
 sudo chown -R $USER:lambda-mpi-PG0 tmp/
 sudo chown -R $USER:lambda-mpi-PG0 ../bin/wsk
@@ -30,7 +33,7 @@ ansible-playbook routemgmt.yml
 
 
 # set wsk
-wsk property set --apihost 10.10.1.2
+wsk property set --apihost 172.17.0.1
 wsk property set --auth `cat ~/openwhisk/ansible/files/auth.whisk.system`
 
 wsk sdk install bashauto
